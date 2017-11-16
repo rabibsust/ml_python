536e5c1f97cd245d4748d004bc9c5107630b4435
rabib <rabib@shaficonsul;tancy.com>
2017-11-16 18:25:38 +0600

    Test harnes and build models

--- validation_data.py
+++ validation_data.py
@@ -29,6 +29,27 @@ validation_size = 0.20
 seed = 7
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
 
+#Test Harness
+#Test options and evaluate metric
+scoring = 'accuracy'
+
+#Build Models
+#We are using 6 different algorithms
+#This is mixture of simple linear (LR and LDA), non linear (KNN, CARD, NB and SVM) algorithms
+#We reset the random number seed before each run to ensure that the evaluation of each algorithm is performed using rhe exact data splits
+#It ensures results are directly comparabe
+
+#Spot check algorithms
+
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+
 
 
 
