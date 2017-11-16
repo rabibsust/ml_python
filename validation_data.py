ab56d963be41dcff235cd71a962034ece770591d
rabib <rabib@shaficonsul;tancy.com>
2017-11-16 18:26:40 +0600

    evaluate models, compare algorithms and make predictions

--- validation_data.py
+++ validation_data.py
@@ -49,7 +49,33 @@ models.append(('CART', DecisionTreeClassifier()))
 models.append(('NB', GaussianNB()))
 models.append(('SVM', SVC()))
 
+#evaluate each model in turn
+results = []
+names = []
 
+for name, model in models:
+	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv= kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+
+	
+#compare algorithms
+fig = plt.figure();
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+#plt.show()
+
+#Make prediction on validation dataset
+svm = SVC()
+svm.fit(X_train, Y_train)
+predictions = svm.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
 
 
 
